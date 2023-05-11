a104e9e29d076d87f07673e61bc294e490e8d248
Jaewon Chang <hepachupa@gmail.com>
2023-05-12 01:09:34 +0900

    [unrated] Title: 문자열 돌리기, Time: 2.66 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#define LEN_INPUT 10
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    
+    int length = strlen(s1);
+    
+    for(int i = 0; i < length ; i++){
+        printf("%c\n",s1[i]);
+    }
+
+    return 0;
+}
